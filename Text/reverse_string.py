de07fc8359edacd6d74642a63687a16609d97083
milanlajda <milanlajda1@gmail.com>
2016-08-25 00:05:30 +0200

    finished reverse string

--- Text/reverse_string.py
+++ Text/reverse_string.py
@@ -1,14 +1,10 @@
 def reverse_string(str):
-    #veta=str(a)
-    revstr = []
-    limit = len(str)
-    for x in range (0, len(str)):
-        revstr.append(str[limit-x])
+    revstr=str[::-1]
     print revstr
 
 
 
 
 reverse_string("abcd")
-#reverse_string("12345")
-#reverse_string("asdfghj")
+reverse_string("12345")
+reverse_string("asdfghj")
